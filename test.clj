dd82367fae65724b4d9073aac474e0a24f4031fa
Gstx <geekhous3@outlook.com>
2024-11-06 22:43:56 -0300

    Update test.clj

--- test.clj
+++ test.clj
@@ -3,112 +3,54 @@
    [clojure.set :as set]
    [clojure.string :as str]))
 
-(defn generate-random-set []
-  (set (take (rand-int 10) (repeatedly #(rand-nth [:a :b :c :d :e :f :g :h :i :j])))))
+(defn create-account [id initial-balance]
+  {:id id
+   :balance initial-balance
+   :transactions []})
 
-(defn perform-set-operations [set1 set2]
-  {:intersection (set/intersection set1 set2)
-   :difference (set/difference set1 set2)
-   :union (set/union set1 set2)
-   :symmetric-difference (set/difference (set/union set1 set2) (set/intersection set1 set2))})
+(defn get-balance [account]
+  (:balance account))
 
-(defn create-nested-maps [depth]
-  (if (zero? depth)
-    (generate-random-set)
-    {(keyword (str "level-" depth)) (create-nested-maps (dec depth))}))
+(defn update-balance [account amount]
+  (-> account
+      (update :balance + amount)
+      (update :transactions conj {:amount amount :timestamp (System/currentTimeMillis)})))
 
-(defn deep-merge [& maps]
-  (if (every? map? maps)
-    (apply merge-with deep-merge maps)
-    (last maps)))
+(defn validate-transfer [from-account to-account amount]
+  (and (>= (get-balance from-account) amount)
+       (pos? amount)
+       (not= (:id from-account) (:id to-account))))
 
-(defn transform-sets [input-set]
-  (->> input-set
-       (map #(hash-map % (count (str %))))
-       (reduce merge)))
+(defn transfer [from-account to-account amount]
+  (if (validate-transfer from-account to-account amount)
+    [(update-balance from-account (- amount))
+     (update-balance to-account amount)]
+    [from-account to-account]))
 
-(defn fibonacci [n]
-  (loop [a 0 b 1 series []]
-    (if (>= (count series) n)
-      series
-      (recur b (+ a b) (conj series a)))))
+(defn create-bank []
+  (atom {}))
 
-(defn process-data-structure [input]
-  (letfn [(process-element [elem]
-            (cond
-              (set? elem) (transform-sets elem)
-              (map? elem) (reduce-kv #(assoc %1 %2 (process-element %3)) {} elem)
-              (sequential? elem) (mapv process-element elem)
-              :else elem))]
-    (process-element input)))
+(defn add-account [bank account-id initial-balance]
+  (swap! bank assoc account-id (create-account account-id initial-balance)))
 
-(defn generate-complex-structure []
-  (let [base-sets [(generate-random-set) (generate-random-set) (generate-random-set)]
-        nested-structure (create-nested-maps 5)
-        processed-data (process-data-structure nested-structure)
-        fib-sequence (fibonacci 10)]
-    {:base-sets base-sets
-     :set-operations (map #(perform-set-operations %1 %2) base-sets (rest base-sets))
-     :nested-data processed-data
-     :fibonacci fib-sequence
-     :combined (deep-merge processed-data
-                          {:additional-data (transform-sets (first base-sets))})}))
+(defn get-account [bank account-id]
+  (get @bank account-id))
 
-(defn apply-operations [data]
-  (letfn [(operation-chain [x]
-            (->> x
-                 (map str)
-                 (map #(str/split % #""))
-                 (map frequencies)
-                 (apply merge-with +)))]
-    (->> data
-         (map (fn [[k v]]
-                [k (if (set? v)
-                     (operation-chain v)
-                     v)]))
-         (into {}))))
+(defn process-transfer [bank from-id to-id amount]
+  (swap! bank
+         (fn [current-state]
+           (let [from-account (get current-state from-id)
+                 to-account (get current-state to-id)
+                 [updated-from updated-to] (transfer from-account to-account amount)]
+             (assoc current-state
+                    from-id updated-from
+                    to-id updated-to)))))
 
-(defn recursive-transform [input depth]
-  (if (zero? depth)
-    input
-    (let [transformed (if (map? input)
-                       (update-vals input #(recursive-transform % (dec depth)))
-                       (if (set? input)
-                         (perform-set-operations input (generate-random-set))
-                         input))]
-      (if (map? transformed)
-        (apply-operations transformed)
-        transformed))))
+(def bank (create-bank))
 
-(defn execute-complex-operations []
-  (let [initial-structure (generate-complex-structure)
-        transformed-structure (recursive-transform initial-structure 3)
-        result-sets (map generate-random-set (range 5))
-        combined-results (->> result-sets
-                            (partition 2 1)
-                            (map #(apply perform-set-operations %))
-                            (reduce deep-merge))]
-    {:initial initial-structure
-     :transformed transformed-structure
-     :result-sets result-sets
-     :combined-results combined-results
-     :final-merge (deep-merge transformed-structure
-                             combined-results
-                             {:timestamp (System/currentTimeMillis)})}))
+(add-account bank "alice" 1000)
+(add-account bank "bob" 500)
+(process-transfer bank "alice" "bob" 300)
 
-(defn process-and-aggregate [n]
-  (->> (range n)
-       (map (fn [_] (execute-complex-operations)))
-       (reduce (fn [acc result]
-                (merge-with (fn [a b]
-                             (cond
-                               (and (map? a) (map? b)) (deep-merge a b)
-                               (and (set? a) (set? b)) (set/union a b)
-                               :else b))
-                           acc
-                           result))
-               {})))
-
-(def final-result (process-and-aggregate 3))
-
-(println final-result)
+(println "Alice's balance:" (get-balance (get-account bank "alice")))
+(println "Bob's balance:" (get-balance (get-account bank "bob")))
