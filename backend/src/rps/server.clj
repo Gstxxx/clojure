780828ad31d581a588b284f0ab85c69131fe079b
gstxxx <geekhous3@outlook.com>
2025-04-11 09:14:27 -0300

    Adiciona configuração de middleware no servidor e atualiza .gitignore para incluir diretório de recursos do backend.

--- backend/src/rps/server.clj
+++ backend/src/rps/server.clj
@@ -1,11 +1,20 @@
 (ns rps.server
   (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
             [rps.api :as api]))
 
+(def app
+  (-> api/app
+      (wrap-resource "public")
+      (wrap-content-type)
+      (wrap-defaults site-defaults)))
+
 (defn start-server [port]
-  (jetty/run-jetty api/app {:port port :join? false}))
+  (jetty/run-jetty app {:port port :join? false}))
 
 (defn -main [& args]
   (let [port (or (some-> args first Integer/parseInt) 3000)]
     (start-server port)
-    (println (str "Servidor rodando na porta " port)))) 
+    (println (str "Servidor rodando na porta " port))))
